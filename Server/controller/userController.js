a94f3d48f204c543051916b42be884973ee5a22e
Kelvin Obidozie <kelvinobidozie@programmer.net>
2023-10-20 11:48:19 +0100

    Update user profile

--- Server/controller/userController.js
+++ Server/controller/userController.js
@@ -78,12 +78,28 @@ const getUSerProfile = asyncHandler(async (req, res) => {
 });
 
 
-//? @Update User Profile
-//! @route ---PUT/api/users/profile 
+//? @Put User Profile
+//! @route ---PUT/api/users/updateProfile 
 //* @access Private
+// TODO: Add functionality to handle logout upon profile update
 const updateUserProfile = asyncHandler(async (req, res) => {
-
-  res.status(200).json({ message: "Update Successful" });
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found')
+  }
 });
 
 export { authUSer, registerUSer, logoutUser, getUSerProfile, updateUserProfile }
