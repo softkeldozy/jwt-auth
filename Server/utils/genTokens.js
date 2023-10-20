697c0355174db2c2023005f4da938c7feacad6d4
Kelvin Obidozie <kelvinobidozie@programmer.net>
2023-10-20 05:35:05 +0100

    Configured jwt

---
+++ Server/utils/genTokens.js
@@ -0,0 +1,15 @@
+import { Jwt } from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiration: '30d'
+  });
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'strict',
+    maxAge: 30 * 24 * 60 * 60 * 1000
+  });
+}
+
+export default generateToken
