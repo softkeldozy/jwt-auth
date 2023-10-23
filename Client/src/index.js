042c4bc617874ca547ce9f8fec48d2c3e3c59b57
Kelvin Obidozie <kelvinobidozie@programmer.net>
2023-10-23 04:52:52 +0100

    Add private route component

--- Client/src/index.js
+++ Client/src/index.js
@@ -5,11 +5,13 @@ import store from './store';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import PrivateRoute from './components/privateRoute/PrivateRoute';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeScreen from './components/screens/HomeScreen';
 import LoginScreen from './components/screens/LoginScreen';
 import RegisterScreen from './components/screens/RegisterScreen';
+import ProfileScreen from './components/screens/ProfileScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -17,6 +19,10 @@ const router = createBrowserRouter(
       <Route index={true} path='/' element={<HomeScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
+      {/* PRIVATE ROUTES */}
+      <Route path='' element={<PrivateRoute />} >
+        <Route path='/profile' element={<ProfileScreen />} />
+      </Route>
     </Route>
   )
 )
